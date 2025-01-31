bcaaff8234b6e2536e51e8e1085cf4565d52275e
Ulrich Hetmaniuk <ulrich@shiftinvert.com>
2025-01-30 18:59:34 -0600

    Add new example.

--- src/FunctionExamples.h
+++ src/FunctionExamples.h
@@ -19,4 +19,22 @@ namespace IMSI {
             return double(x * (1.0 - x) * y * (1.0 - y)); };
     };
 
+    /// \brief Set of functions for 2D oscillatory problem
+    /// This problem is used in Le Bris, Legoll, Lozinski,
+    /// "MsFEM a la Crouzeix-Raviart for Highly Oscillatory Elliptic Problems"
+    /// (2013).
+    struct OscillatoryScalarPb {
+        const std::function<double(double, double, double)> f = [](double x, double y, [[maybe_unused]] double) -> double {
+            return double(sin(x) * sin(y)); };
+        const std::function<double(double, double, double)> ax = [](double x, double y, [[maybe_unused]] double) -> double {
+            auto const c = cos(150 * x);
+            auto const s = sin(150 * y);
+            return double(1 + 100 * c * c * s * s); };
+        const std::function<double(double, double, double)> ay = [](double x, double y, [[maybe_unused]] double) -> double {
+            auto const c = cos(150 * x);
+            auto const s = sin(150 * y);
+            return double(1 + 100 * c * c * s * s); };
+        const std::optional< std::function<double(double, double, double)> > u = nullopt;
+    };
+
 }
